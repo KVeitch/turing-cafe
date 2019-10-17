9d5bc242db20c6a9b2ab6b9464add394bd4288be
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-17 11:24:23 -0600

    Add testing for Form handleChange

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -14,7 +14,6 @@ class Form extends Component {
   }
 
   handleChange(event) {
-    console.log(event)
     this.setState({ [event.target.name]:event.target.value })
   }
 
@@ -24,7 +23,7 @@ class Form extends Component {
   }
 
   render() {
-    console.log(this.props)
+
     return (
       <form>
         <input 
--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -3,18 +3,71 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme'
 import Form from './Form';
 
-describe('App',()=>{
+describe('Form',()=>{
   let wrapper;
   
   beforeEach(()=> {
-    wrapper = shallow(<App />)
+    wrapper = shallow(<Form />)
   })
 
   it('should match the snapshot with all the data passed in correctly', () => {
     expect(wrapper).toMatchSnapshot();
   });
+  describe('handleChange', ()=>{
 
-  
+    it('should change state.name when handleChange is called from name',()=>{
+      const event = {
+        target:{
+          name:'name',
+          value:'Bob'
+        } 
+       }
+
+      wrapper.instance().handleChange(event)
+      expect(wrapper.state().name).toEqual('Bob') 
+    })
+
+    it('should change state.date when handleChange is called from date',()=>{
+      const event = {
+        target:{
+          name:'date',
+          value:'12/12'
+        } 
+       }
+       wrapper.instance().handleChange(event)
+       expect(wrapper.state().date).toEqual('12/12')
+    })
+
+    it('should change state.time when handleChange is called from time',()=>{
+      const event = {
+        target:{
+          name:'time',
+          value:'7:00'
+        } 
+       }
+       wrapper.instance().handleChange(event)
+       expect(wrapper.state().time).toEqual('7:00')
+    })
+
+    it('should change state.number when handleChange is called from number',()=>{
+      const event = {
+        target:{
+          name:'number',
+          value:36
+        } 
+       }
+       wrapper.instance().handleChange(event)
+       expect(wrapper.state().number).toEqual(36)
+    })
+  })
 
 
+
+  // it('',()=>{
+    
+  // })
+
+  // it('',()=>{
+    
+  // })
 })
