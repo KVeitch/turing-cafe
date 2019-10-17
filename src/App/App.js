ad9eee6f0d47158da1934358457ab97736da7f4a
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-17 10:08:38 -0600

    Add form state changes

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends Component {
   constructor() {
     super()
-    this.state= {
+    this.state = {
       reservations:[]
     }
   }
--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,15 +4,27 @@ import './Form.css';
 class Form extends Component {
   constructor() {
     super()
+    this.state= {
+      name : '',
+      date: '',
+      time: '',
+      numGuest:'',
 
+    }
+  }
+
+  handleChange(event) {
+    console.log(event)
+    this.setState({ [event.target.name]:event.target.value })
   }
 
   render() {
     return (
       <>
-        <input />
-        <input />
-        <input />
+        <input name='name' value={this.state.name} onChange={(event)=>this.handleChange(event)}/>
+        <input name='date' value={this.state.date} onChange={(event)=>this.handleChange(event)}/>
+        <input name='time' value={this.state.time} onChange={(event)=>this.handleChange(event)}/>
+        <input name='numGuest' value={this.state.numGuest} onChange={(event)=>this.handleChange(event)}/>
         <div type="button"> Set Reservation</div>
       </>
     )
